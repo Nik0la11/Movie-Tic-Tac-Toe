0a0395a71a6baf526782306f60b3c951df72c453
Nik0la11 <marcetanikola3@gmail.com>
2026-08-03 00:29:35 +0200

    Implement movie search results display

--- app/singleplayer/page.tsx
+++ app/singleplayer/page.tsx
@@ -11,6 +11,12 @@ interface Cell {
   claimedBy: "X" | "O" | null;
 }
 
+interface Movie {
+  id: number;
+  title: string;
+  poster_path: string;
+}
+
 const SinglePlayerGame = () => {
   const [gridData, setGridData] = useState<{
     rows: string[];
@@ -28,7 +34,7 @@ const SinglePlayerGame = () => {
 
   const [isCellClicked, setIsCellClicked] = useState(false);
   const [searchValue, setSearchValue] = useState("");
-  const [searchMovies, setSearchMovies] = useState("");
+  const [searchMovies, setSearchMovies] = useState<Movie[]>([]);
 
   const handleClick = (cellId: number) => {
     console.log(`Kliknuto je polje sa ID: ${cellId}`);
@@ -59,12 +65,16 @@ const SinglePlayerGame = () => {
         `/api/movies?endpoint=${endpoint}&params=${encodeURIComponent(params)}`
       );
       const data = await res.json();
-
-      console.log(`rezulati pretrage:`, data.results);
+      setSearchMovies(data.results);
     };
 
     fetchMovies();
   }, [searchValue]);
+
+  const handleClose = () => {
+    setIsCellClicked(false);
+    setSearchValue("");
+  };
 
   if (!gridData) {
     return <div className="h-screen overflow-hidden"></div>;
@@ -128,7 +138,7 @@ const SinglePlayerGame = () => {
       {isCellClicked ? (
         <div
           className="fixed inset-0 flex items-center justify-center bg-white/50"
-          onClick={() => setIsCellClicked(false)}
+          onClick={() => handleClose()}
         >
           <div
             className="bg-slate-800 px-8 py-8 flex flex-col items-center justify-center relative rounded-md w-96"
@@ -136,7 +146,7 @@ const SinglePlayerGame = () => {
           >
             <button
               className="absolute top-3 right-3 cursor-pointer"
-              onClick={() => setIsCellClicked(false)}
+              onClick={() => handleClose()}
             >
               X
             </button>
@@ -147,10 +157,41 @@ const SinglePlayerGame = () => {
               placeholder="Search for movies..."
               value={searchValue}
               onChange={(e) => setSearchValue(e.target.value)}
+              autoComplete="off"
               className="rounded-md p-2 bg-slate-600 w-full grow mb-4"
             />
 
-            {searchValue ? <div></div> : <></>}
+            {searchValue ? (
+              searchMovies.length > 0 ? (
+                <div className="flex flex-col items-start h-[243px] overflow-y-auto mb-4 rounded-md border border-slate-400 w-full">
+                  {searchMovies.map((searchMovie) => (
+                    <button
+                      key={searchMovie.id}
+                      className="flex justify-start items-center p-2 gap-2 cursor-pointer border-b border-b-slate-400 w-full"
+                    >
+                      {searchMovie.poster_path ? (
+                        <img
+                          src={`https://image.tmdb.org/t/p/w92${searchMovie.poster_path}`}
+                          alt={searchMovie.title}
+                          className=" h-16 aspect-[2/3] object-cover rounded-sm"
+                        />
+                      ) : (
+                        <div className="h-16 aspect-[2/3] bg-slate-600 rounded-sm flex flex-col items-center justify-center text-[10px] text-slate-400 font-bold border border-slate-500/30 shrink-0 select-none">
+                          🎬 <span>NO PIC</span>
+                        </div>
+                      )}
+                      <p>{searchMovie.title}</p>
+                    </button>
+                  ))}
+                </div>
+              ) : (
+                <div className="flex justify-center items-center p-2 border border-slate-400 w-full text-slate-400 mb-4 rounded-md">
+                  <p>No results found.</p>
+                </div>
+              )
+            ) : (
+              <></>
+            )}
 
             <button
               className="text-white bg-slate-400 p-2 w-full rounded-md cursor-not-allowed hover:bg-green-500 cursor-pointer"
