a93e2d5b04fd98c48b99ebdbb15faf665f630f9d
Nik0la11 <marcetanikola3@gmail.com>
2026-08-06 13:01:02 +0200

    Update page.tsx

--- app/room/page.tsx
+++ app/room/page.tsx
@@ -25,35 +25,35 @@ const Room = () => {
             <h2 className="text-xl sm:text-2xl">Create Room: Settings</h2>
           </div>
 
-          <div className="flex items-center justify-center mb-2">
+          <div className="flex items-center justify-start mb-2">
             <div className="rounded-l-md border border-white pr-4 w-26 pl-2 sm:pr-12 py-2 sm:w-36">
-              Best of 3
+              <p className="text-sm sm:text-base">Best of 3</p>
             </div>
             <div className="border-y-1 border-r-1 pr-4 w-26 pl-2 sm:pr-12 py-2 sm:w-36">
-              Best of 5
+              <p className="text-sm sm:text-base">Best of 5</p>
             </div>
             <div className="border-y-1 pr-4 w-26 pl-2 sm:pr-12 py-2 sm:w-36">
-              Best of 7
+              <p className="text-sm sm:text-base">Best of 7</p>
             </div>
             <div className="rounded-r-md border border-white pr-4 w-26 pl-2 sm:pr-12 py-2 sm:w-36">
-              Unlimited
+              <p className="text-sm sm:text-base">Unlimited</p>
             </div>
           </div>
-          <div className="flex items-center justify-center mb-4">
+          <div className="flex items-center justify-start mb-4">
             <div className="rounded-l-md border border-white pr-4 w-26 pl-2 sm:pr-12 py-2 sm:w-36">
-              <h4>Anti Cheat</h4>
+              <h4 className="text-sm sm:text-base">Anti Cheat</h4>
               <p className="text-gray-400 text-xs">15s turns</p>
             </div>
             <div className="border-y-1 border-r-1 pr-4 w-26 pl-2 sm:pr-12 py-2 sm:w-36">
-              <h4>Normal</h4>
+              <h4 className="text-sm sm:text-base">Normal</h4>
               <p className="text-gray-400 text-xs">40s turns</p>
             </div>
             <div className="border-y-1 pr-4 w-26 pl-2 sm:pr-12 py-2 sm:w-36">
-              <h4>Tactician</h4>
+              <h4 className="text-sm sm:text-base">Tactician</h4>
               <p className="text-gray-400 text-xs">60s turns</p>
             </div>
             <div className="rounded-r-md border border-white pr-4 w-26 pl-2 sm:pr-12 py-2 sm:w-36">
-              <h4>Unlimited</h4>
+              <h4 className="text-sm sm:text-base">Unlimited</h4>
               <p className="text-gray-400 text-xs">No limit</p>
             </div>
           </div>
